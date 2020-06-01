eb83e0a9f619bf2dbe9508f6c16e1483809ca1d8
prnjl02 <verma.pranjalraj@gmail.com>
2020-06-01 09:53:02 +0530

    final

---
+++ login/src/app/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+//import { AuthService } from '../auth.service';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  // constructor(private router:Router , private authService:AuthService) { }
+constructor(private router:Router){}
+
+  ngOnInit(): void {
+  }
+
+  submit(loginDetails){
+    console.log('loggedin',loginDetails.value);
+    //this.authService.loginDetails(loginDetails.value)
+       this.router.navigate(['/dashboard'])
+      }
+}
